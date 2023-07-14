e607955e074d6b3f92b805d2732cb2f48f3d7723
vetonxhumkar890@outlook.com <vetonxhumkar890@outlook.com>
2023-07-14 17:33:08 +0200

    Product Details, Categories and Seeding Db

--- BlazorEcommerce/Server/Data/DataContext.cs
+++ BlazorEcommerce/Server/Data/DataContext.cs
@@ -10,6 +10,26 @@
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Category>().HasData(
+                new Category
+                {
+                    Id = 1,
+                    Name = "Books",
+                    Url = "books"
+                },
+                new Category
+                {
+                    Id = 2,
+                    Name = "Movies",
+                    Url = "movies"
+                },
+                new Category
+                {
+                    Id = 3,
+                    Name = "Video games",
+                    Url = "video-games"
+                }
+            );
             modelBuilder.Entity<Product>().HasData(
                 new Product
                 {
@@ -17,7 +37,8 @@
                     Title = "Hitchhiker Guide",
                     Description = "The Hitchhiker's Guide to the Galaxy[a][b] is a comedy science fiction franchise created by Douglas Adams. Originally a 1978 radio comedy broadcast on BBC Radio 4, it was later adapted to other formats, including novels, stage shows, comic books, a 1981 TV series, a 1984 text-based video game, and 2005 feature film.",
                     ImageURL = "https://upload.wikimedia.org/wikipedia/en/7/74/Ready_Player_One_%28film%29.png",
-                    Price = 9.99m
+                    Price = 9.99m,
+                    CategoryId = 1
                 },
                 new Product
                 {
@@ -25,7 +46,9 @@
                     Title = "Ready player one",
                     Description = "asdasdasdasdasdasdasdaasdasdasdasdasdasdaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
                     ImageURL = "https://upload.wikimedia.org/wikipedia/en/7/74/Ready_Player_One_%28film%29.png",
-                    Price = 19.99m
+                    Price = 19.99m,
+                    CategoryId = 1
+
                 },
                 new Product
                 {
@@ -33,10 +56,32 @@
                     Title = "Nineteen eighty four",
                     Description = "xzcccccccccccccccccccccccccccccccccccccccccccccccccccccccc",
                     ImageURL = "https://upload.wikimedia.org/wikipedia/en/7/74/Ready_Player_One_%28film%29.png",
-                    Price = 29.99m
+                    Price = 29.99m,
+                    CategoryId = 1
+
+                },
+                new Product
+                {
+                    Id = 4,
+                    Title = "CategoryTest",
+                    Description = "xzcccccccccccccccccccccccccccccccccccccccccccccccccccccccc",
+                    ImageURL = "https://upload.wikimedia.org/wikipedia/en/7/74/Ready_Player_One_%28film%29.png",
+                    Price = 29.99m,
+                    CategoryId = 2
+
+                },
+                new Product
+                {
+                    Id = 5,
+                    Title = "CategoryTest",
+                    Description = "xzcccccccccccccccccccccccccccccccccccccccccccccccccccccccc",
+                    ImageURL = "https://upload.wikimedia.org/wikipedia/en/7/74/Ready_Player_One_%28film%29.png",
+                    Price = 29.99m,
+                    CategoryId = 3
                 }
             );
         }
         public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
     }
 }
---
+++ BlazorEcommerce/Server/Migrations/20230714153157_CategorySeed.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BlazorEcommerce.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class CategorySeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "Description", "ImageURL", "Price", "Title" },
+                values: new object[,]
+                {
+                    { 4, 2, "xzcccccccccccccccccccccccccccccccccccccccccccccccccccccccc", "https://upload.wikimedia.org/wikipedia/en/7/74/Ready_Player_One_%28film%29.png", 29.99m, "CategoryTest" },
+                    { 5, 3, "xzcccccccccccccccccccccccccccccccccccccccccccccccccccccccc", "https://upload.wikimedia.org/wikipedia/en/7/74/Ready_Player_One_%28film%29.png", 29.99m, "CategoryTest" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
